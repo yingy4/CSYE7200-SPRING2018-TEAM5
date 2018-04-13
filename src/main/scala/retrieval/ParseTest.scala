b7f6feca23eee7f8c128c25a02cf07a4f522a518
HouzeLiu <liu.hou@husky.neu.edu>
2018-04-13 17:01:58 -0400

    update

--- src/main/scala/retrieval/ParseTest.scala
+++ src/main/scala/retrieval/ParseTest.scala
@@ -2,7 +2,7 @@ package retrieval
 
 import java.io.{BufferedWriter, FileWriter}
 
-import retrieval.UseCases.{Top_10_Prices, Top_K_Brands, Top_K_Colors, buf}
+import retrieval.UseCases.{Top_k_p,Top_K_Prices, Top_K_Brands, Top_K_Colors, buf}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
@@ -15,7 +15,12 @@ object ParseTest extends App {
 
   Top_K_Colors(10).foreach(println)
   Top_K_Brands(10).foreach(println)
-  Top_10_Prices().foreach(println)
+  print(Top_k_p())//normal prices in String format
+
+  //k-means deal with prices
+//  Top_K_Prices().foreach(println)
+
+
 
   val file4 = "item.txt"
   val writer4 = new BufferedWriter(new FileWriter(file4, true))
@@ -23,6 +28,8 @@ object ParseTest extends App {
     writer4.append(x + "\n")
   }
   writer4.close()
+
+
 
 
 }
